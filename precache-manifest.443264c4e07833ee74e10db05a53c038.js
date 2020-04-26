07bf356e65fae7eb954abd016e598b9ebbf29d3d
Sahand Masoleh <sahand.masoleh@gmail.com>
2020-04-27 03:56:32 +0800

    Updates

---
+++ precache-manifest.443264c4e07833ee74e10db05a53c038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6e688a75ac01a6952cbae53629ab37",
+    "url": "/project-quote-machine/index.html"
+  },
+  {
+    "revision": "bb6520b837caf1563a13",
+    "url": "/project-quote-machine/static/css/main.abafa9c6.chunk.css"
+  },
+  {
+    "revision": "79f41aadfbd84ce59725",
+    "url": "/project-quote-machine/static/js/2.8b2608d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-quote-machine/static/js/2.8b2608d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6520b837caf1563a13",
+    "url": "/project-quote-machine/static/js/main.550e340a.chunk.js"
+  },
+  {
+    "revision": "b76ed1fa46f60529153c",
+    "url": "/project-quote-machine/static/js/runtime-main.bb905d48.js"
+  }
+]);
